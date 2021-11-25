bc54b5f39cb3bca9d71d4f754f68bdacc335c509
AzureAD\EliasFransson <46138@gymnasium.ax>
2021-11-25 11:46:27 +0200

    La till 5.1.svar.php

---
+++ Kap1/5.1.svar.php
@@ -0,0 +1,14 @@
+<?php
+
+$namn=$_GET['namn'];
+$adress=$_GET['adress'];
+$postnr=$_GET['postnr'];
+$ort=$_GET['ort'];
+
+echo "Du skrev in följande:<br>";
+echo "Namn: <b>$namn</br><br>";
+echo "Adress: <b>$adress</br><br>";
+echo "Postnr: <b>$postnr</br><br>";
+echo "Ort: <b>$ort</br><br>";
+
+
