c7b822b06662c143c63f92e5888e41d636f3a621
AzureAD\EliasFransson <46138@gymnasium.ax>
2021-11-25 11:49:10 +0200

    la till 5.11.svar.php

---
+++ Kap1/5.11.svar.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$namn=$_POST['namn'];
+$fodelsedag=$_POST['fodelsedag'];
+$user=$_POST['user'];
+$losen=$_POST['losen'];
+$telefon=$_POST['telefon'];
+
+echo "Du skrev in följande: <br>";
+echo "namn: <b>$namn</br><br>";
+echo "fodelsedag: <b>$fodelsedag</br><br>";
+echo "user: <b>$user</br><br>";
+echo "losen: <b>$losen</br><br>";
+echo "telefon: <b>$telefon</br><br>";
+
+
+
+
