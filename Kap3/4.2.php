1cfc8316165fc295e2e645ffde6189b841711ed2
AzureAD\EliasFransson <46138@gymnasium.ax>
2021-11-25 12:05:57 +0200

    La till 4.2.php

---
+++ Kap3/4.2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="sv">
+	
+	<head>
+		<meta charset="UTF-8">
+		<title>CamelCaseFunktion</title>
+	</head>
+		<body>
+			<h1>Skapa CamelCaseFunktion</h1>
+			<form method="POST">
+				Text <input type="text" name="text"><br>
+				<input type="submit" value="Skicka">
+				<input type="reset" value="Rensa">
+			</form>
+		</body>
+</html>
+	
